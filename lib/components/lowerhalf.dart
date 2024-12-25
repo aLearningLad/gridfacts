fc573dec01e6f037a63b09347b7ea6fddb898177
aLearningLad <mocmanca@gmail.com>
2024-12-25 11:28:27 +0200

    adding questionsList logic and state to lower half

--- lib/components/lowerhalf.dart
+++ lib/components/lowerhalf.dart
@@ -1,21 +1,61 @@
 import 'package:flutter/material.dart';
 
-class LowerHalf extends StatelessWidget {
+class LowerHalf extends StatefulWidget {
   const LowerHalf({super.key});
 
   @override
+  State<LowerHalf> createState() => _LowerHalfState();
+}
+
+class _LowerHalfState extends State<LowerHalf> {
+  @override
   Widget build(BuildContext context) {
+    final List<Map<String, String>> questionsList = [
+      {"Who Monaco GP most times?": "Ayrton!"},
+      {"Most race wins?": "Lewis!"},
+      {"Human Error Champ?": "Max Verstappen"},
+      {"Who is a snake in the paddock?": "Toto Wolff"},
+    ];
+
     return Container(
-        height: 250,
+        padding: EdgeInsets.all(10),
+        height: 300,
         width: double.infinity,
         decoration: BoxDecoration(
             borderRadius: BorderRadius.circular(12),
             color: Colors.amber[
                 100]), // make this container colour change when the question changes
         child: Column(
+          spacing: 5,
           children: [
-            Text("Race wins . . ."),
-            Text("WHich driver won Monaco the most times")
+            Row(
+              mainAxisSize: MainAxisSize.max,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  "On Race Wins . . .",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(fontSize: 20),
+                )
+              ],
+            ),
+            Container(
+              height: 225,
+              child: SingleChildScrollView(
+                child: Column(
+                  mainAxisSize: MainAxisSize.max,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Text(
+                      "Which driver won Monaco the most times Which driver won Monaco the most times Which driver won Monaco the most times Which driver won Monaco the most times Which driver won Monaco the most times Which driver won Monaco the most times Which driver won Monaco the most times Which driver won Monaco the most times Which driver won Monaco the most times Which driver won Monaco the most times Which driver won Monaco the most times Which driver won Monaco the most times Which driver won Monaco the most times Which driver won Monaco the most times Which driver won Monaco the most times Which driver won Monaco the most times Which driver won Monaco the most times Which driver won Monaco the most times",
+                      textAlign: TextAlign.center,
+                      style: TextStyle(fontSize: 16),
+                    ),
+                  ],
+                ),
+              ),
+            )
           ],
         ));
   }
